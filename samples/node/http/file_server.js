87044100ecaf1dd7bc862cfcdf1901f77326ec46
AlannnnnnL <lhf_99280@163.com>
2019-02-19 17:28:02 +0800

    http请求为提供具体文件时，默认加载

--- samples/node/http/file_server.js
+++ samples/node/http/file_server.js
@@ -1,34 +1,46 @@
 'use strict';
-
-// a simple http server
+// 文件服务器
 
 var
     fs = require('fs'),
     url = require('url'),
     path = require('path'),
     http = require('http');
-
 var root = path.resolve(process.argv[2] || '.');
-
 console.log('Static root dir: ' + root);
 
+// 创建服务器
 var server = http.createServer(function (request, response) {
-    var
-        pathname = url.parse(request.url).pathname, // '/static/bootstrap.css'
-        filepath = path.join(root, pathname); // '/srv/www/static/bootstrap.css'
-    fs.stat(filepath, function (err, stats) {
-        if (!err && stats.isFile()) {
-            console.log('200 ' + request.url);
-            response.writeHead(200);
-            fs.createReadStream(filepath).pipe(response);
-        } else {
-            console.log('404 ' + request.url);
-            response.writeHead(404);
-            response.end('404 Not Found');
-        }
-    });
+    // 获得URL的path，类似'/css/bootstrap.css';
+    var pathname = url.parse(request.url).pathname;
+    console.log('pathname:' + pathname);
+    // 获取对应的本地文件路径, 类似'/src/www/css/bootstrap.css';
+    var filepath = path.join(root, pathname);
+    console.log('filepath:' + filepath);
+    // 默认查找的文件
+    var defaultFile = ['default.html', 'index.html'];
+    readFile(filepath);
+    function readFile(filepath) {
+        // 获取文件状态;
+        fs.stat(filepath, function (err, stats) {
+            if (!err && stats.isFile()) {
+                // 没有错误并且文件存在
+                // 发送200响应
+                response.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+                // 将文件流导向response
+                fs.createReadStream(filepath).pipe(response);
+            } else if (defaultFile.length > 0) {
+                // 目录存在,默认搜索sample.txt
+                filepath = path.join(root, defaultFile.shift(1));
+                console.log('默认filepath:' + filepath);
+                readFile(filepath);
+            } else {
+                // 出错了或者文件不存在
+                response.writeHead(404);
+                response.end('404 Not Found');
+            }
+        });
+    }
 });
-
 server.listen(8080);
-
 console.log('Server is running at http://127.0.0.1:8080/');
